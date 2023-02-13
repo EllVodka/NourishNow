f65fa5cff95a1ae148b7ce0b38a219f2b7edbba2
EllVodka <eric.sergueev@gmail.com>
2023-02-13 23:34:43 +0100

    style: add css for register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -19,18 +19,11 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
+                'label'=>'Mot de passe',
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
@@ -44,7 +37,9 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('Ajouter', SubmitType::class)
+            ->add('Ajouter', SubmitType::class,[
+                "label"=>"Continuer",
+            ])
         ;
     }
 
