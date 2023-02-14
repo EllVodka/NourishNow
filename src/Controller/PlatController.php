70923de3f77efe68bf6d6f5a2e16c95cbe9d0a7c
EllVodka <eric.sergueev@gmail.com>
2023-02-14 11:35:49 +0100

    feat(plat): add page for view and add plat

---
+++ src/Controller/PlatController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Plat;
+use App\Form\PlatType;
+use App\Repository\PlatRepository;
+use App\Repository\RestaurantRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/plat")
+ * @IsGranted("ROLE_RESTAURATEUR")
+ */
+class PlatController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_plat")
+     */
+    public function index(PlatRepository $plat): Response
+    {
+        return $this->render('plat/index.html.twig', [
+            'plats' => $plat->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_plat_view")
+     */
+    public function view(int $id,PlatRepository $plat): Response
+    {
+        return $this->render('plat/view.html.twig', [
+            'plat' => $plat->find($id),
+        ]);
+    }
+
+    /**
+     * @Route("/add/{idResto}", name="app_plat_add")
+     * 
+     */
+    public function add(int $idResto, Request $request, RestaurantRepository $restaurantRepository, ManagerRegistry $doctrine): Response
+    {
+        $plat = new Plat();
+
+        $form = $this->createForm(PlatType::class, $plat);
+        $manager = $doctrine->getManager();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plat = $form->getData();
+
+            $restaurant = $restaurantRepository->find($idResto);
+            $plat->setFkRestaurant($restaurant);
+
+            $manager->persist($plat);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_plat');
+        }
+
+        return $this->render('plat/add.html.twig', [
+            'platForm' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/PlatType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Plat;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlatType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('description')
+            ->add('tarif')
+            ->add('Ajouter', SubmitType::class,[
+                "label"=>"Continuer",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Plat::class,
+        ]);
+    }
+}
